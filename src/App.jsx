25569e2c677bcdc53f92a53150f43236e4bc0649
abdulla056 <115686255+abdulla056@users.noreply.github.com>
2025-03-25 11:48:58 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import "./App.css";
 import { initialText } from "./assets/data";
 import { AnimatePresence, motion } from "framer-motion";
 import initialBackground from "./assets/initial_background.svg";
+import WalkingComponent from "./WalkingComponent";
 
 function App() {
   const [textIndex, setTextIndex] = useState(0);
+  const [walking, setWalking] = useState(false);
 
   function updateTextIndex() {
     setTextIndex((prevIndex) => {
       if (prevIndex === initialText.length - 1) {
-        return 0;
+        return prevIndex;
       } else {
         return prevIndex + 1;
       }
@@ -23,27 +25,56 @@ function App() {
       style={{ backgroundImage: `url(${initialBackground})` }}
     >
       <div className="items-center gap-6 flex flex-col absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <AnimatePresence mode="wait">
-          <motion.div
-            onClick={updateTextIndex}
-            initial={{ opacity: 0, y: 100 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -100 }}
-            transition={{ duration: 0.5 }}
-            key={textIndex}
-            className=" text-white text-center text-3xl"
-          >
-            {initialText[textIndex]}
-          </motion.div>
+        <AnimatePresence>
+          {walking && (
+            <motion.div
+              initial={{ opacity: 0, y: 100 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -100 }}
+              transition={{ duration: 0.5, delay: 0.7 }}
+            >
+              <WalkingComponent/>
+            </motion.div>
+          )}
         </AnimatePresence>
-        <motion.div
-          className="text-white text-lg font-semibold cursor-pointer"
-          initial={{ opacity: 1 }}
-          animate={{ y: [0, 10, 0], opacity: 1 }}
-          transition={{ duration: 1.5, repeat: Infinity }}
-        >
-          Click to Continue
-        </motion.div>
+        <AnimatePresence>
+          {!walking && (
+            <motion.div
+              initial={{ opacity: 0, y: 100 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -100 }}
+              transition={{ duration: 0.5 }}
+            >
+              <AnimatePresence mode="wait">
+                <motion.div
+                  onClick={updateTextIndex}
+                  initial={{ opacity: 0, y: 100 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -100 }}
+                  transition={{ duration: 0.5 }}
+                  key={textIndex}
+                  className=" text-white text-center text-3xl"
+                >
+                  {initialText[textIndex]}
+                </motion.div>
+              </AnimatePresence>
+              <AnimatePresence mode="wait">
+                {textIndex === initialText.length - 1 && (
+                  <motion.div
+                    className="text-white text-lg font-semibold cursor-pointer"
+                    initial={{ opacity: 0, y: -7 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: -10 }}
+                    transition={{ duration: 0.5, delay: 1 }}
+                    onClick={() => setWalking(true)}
+                  >
+                    Click here to start walking!
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </div>
   );
---
+++ src/WalkingComponent.jsx
@@ -0,0 +1,5 @@
+export default function WalkingComponent() {
+    return (
+        <div>Walking</div>
+    )
+}
