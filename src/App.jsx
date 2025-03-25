0bcfb1314935ce179f01321c699f0ca77adbb62e
abdulla056 <115686255+abdulla056@users.noreply.github.com>
2025-03-25 13:16:11 +0530

    dayum

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import initialBackground from "./assets/initial_background.svg";
 import WalkingComponent from "./WalkingComponent";
 import rabbit from "./assets/rabbit.png";
 import rabbit2 from "./assets/rabbit-2.png";
+import flower1 from "./assets/flower-1.svg";
+import flower2 from "./assets/flower-2.svg";
+import flower3 from "./assets/flower-3.svg";
+import rabbit3 from "./assets/rabbit-3.png";
 
 function App() {
   const [textIndex, setTextIndex] = useState(0);
@@ -41,7 +45,21 @@ function App() {
         )}
       </AnimatePresence>
       <AnimatePresence>
-        {textIndex === initialText.length - 1 && (
+        {textIndex === initialText.length - 1 && !walking && (
+          <motion.img
+            initial={{ opacity: 0, x: 150 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, y: -100 }}
+            transition={{ duration: 0.5, delay: 1.5 }}
+            key={textIndex}
+            src={rabbit3}
+            alt="rabbit"
+            className="size-36 fixed top-0 translate-x-1/2 scale-y-[-1]"
+          />
+        )}
+      </AnimatePresence>
+      <AnimatePresence>
+        {textIndex === initialText.length - 1 && !walking && (
           <motion.img
             initial={{ opacity: 0, x: 150 }}
             animate={{ opacity: 1, x: 0 }}
@@ -51,6 +69,48 @@ function App() {
             src={rabbit}
             alt="rabbit"
             className="w-48 h-48 fixed bottom-36 right-0 translate-x-1/2 mr-16"
+          />
+        )}
+      </AnimatePresence>
+      <AnimatePresence>
+        {textIndex === initialText.length - 2 && (
+          <motion.img
+            initial={{ opacity: 0, x: 150 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, y: -100 }}
+            transition={{ duration: 0.5, delay: 1.5 }}
+            key={textIndex}
+            src={flower1}
+            alt="flower"
+            className="size-42 fixed bottom-36 right-0 translate-x-1/2 mr-16"
+          />
+        )}
+      </AnimatePresence>
+      <AnimatePresence>
+        {textIndex === initialText.length - 2 && (
+          <motion.img
+            initial={{ opacity: 0, y: 150 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -100 }}
+            transition={{ duration: 0.5, delay: 1.5 }}
+            key={textIndex}
+            src={flower2}
+            alt="flower"
+            className="w-48 h-48 fixed bottom-0 translate-x-1/2 mr-16"
+          />
+        )}
+      </AnimatePresence>
+      <AnimatePresence>
+        {textIndex === initialText.length - 2 && (
+          <motion.img
+            initial={{ opacity: 0, y: -150 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -100 }}
+            transition={{ duration: 0.5, delay: 1.5 }}
+            key={textIndex}
+            src={flower3}
+            alt="flower"
+            className="w-48 h-48 fixed top-0 translate-x-1/2 mr-16"
           />
         )}
       </AnimatePresence>
@@ -67,6 +127,9 @@ function App() {
             </motion.div>
           )}
         </AnimatePresence>
+
+
+        {/* This is the initial text */}
         <AnimatePresence>
           {!walking && (
             <motion.div
--- src/WalkingComponent.jsx
+++ src/WalkingComponent.jsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
+import walking from "./assets/walking.gif";
 
 export default function WalkingComponent() {
   const [start, setStart] = useState(true);
+  const sendMessage = () => {
+    const phoneNumber = "+600196381343";
+    const message = encodeURIComponent("Hey Dudul Bozo! I'm starting to walk!");
+    window.open(`https://wa.me/${phoneNumber}?text=${message}`, "_blank");
+  };
   return (
     <>
       <AnimatePresence>
@@ -18,7 +24,10 @@ export default function WalkingComponent() {
               Now you can start walking! Thank you for going through the boring
               journey!
             </span>
-            <button>Click to send Dudul Bozo a message that you're starting to walk, so that you'll never have to walk without company</button>
+            <button onClick={sendMessage}>
+              Click to send Dudul Bozo a message that you're starting to walk,
+              so that you'll never have to walk without company
+            </button>
           </motion.div>
         )}
       </AnimatePresence>
