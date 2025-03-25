05feefe32756aaf06928161c1a8778d0e714735a
abdulla056 <115686255+abdulla056@users.noreply.github.com>
2025-03-25 11:12:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import "./App.css";
+import { initialText } from "./assets/data";
+import { AnimatePresence, motion } from "framer-motion";
+import initialBackground from "./assets/initial_background.svg";
+
+function App() {
+  const [textIndex, setTextIndex] = useState(0);
+  const bgUrl = "src/assets/initial_background.svg";
+
+  function updateTextIndex() {
+    setTextIndex((prevIndex) => {
+      if (prevIndex === initialText.length - 1) {
+        return 0;
+      } else {
+        return prevIndex + 1;
+      }
+    });
+  }
+
+  return (
+    <div
+      className={`w-full h-screen bg-[url('${bgUrl}')] bg-cover bg-center opacity-75`}
+    >
+      <div className="items-center gap-6 flex flex-col absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <AnimatePresence mode="wait">
+          <motion.div
+            onClick={updateTextIndex}
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -100 }}
+            transition={{ duration: 0.5 }}
+            key={textIndex}
+            className=" text-white text-center text-5xl"
+          >
+            {initialText[textIndex]}
+          </motion.div>
+        </AnimatePresence>
+        <motion.div
+          className="text-white text-lg font-semibold cursor-pointer"
+          initial={{ opacity: 1 }}
+          animate={{ y: [0, 10, 0], opacity: 1 }}
+          transition={{ duration: 1.5, repeat: Infinity }}
+        >
+          Click to Continue
+        </motion.div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/data.js
@@ -0,0 +1,10 @@
+export const initialText = [
+    "Hey there, Anoushka! So you had a really long day, and I'm so proud of you for making it through. I know it was tough, but you're tougher.",
+    "I just wanted to make this cool little animation to show appreciation for you.",
+    "It's not the best or most polished, but I hope it brings a smile to your face.",
+    "Amazing job in the studio today! And you managed to go the gym as well! INSANE!, After all that you still managed to get your report done as well, absolutely amazing!",
+]
+
+export const walkingText = [
+    "I believe in retribution, but I also believe in second chances.",
+]
